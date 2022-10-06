cf085ebd8e2c86aa92398fc79857665de9ddc693
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-06 18:34:50 +0600

    :tada: second task completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Vimeo from '@vimeo/player'
+import _ from 'lodash';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+function onPlay(data) {
+  localStorage.setItem("videoplayer-current-time", `${data.seconds}`)
+}
+
+player.on('play', function() {
+  console.log('played the video!');
+});
+player.on('timeupdate', _.throttle(onPlay, 1000));
+
+player.setCurrentTime(`${localStorage.getItem("videoplayer-current-time")}`).catch(function(error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+    default:
+      break;
+  }
+});
