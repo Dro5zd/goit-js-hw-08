2fb23065665dfaf305c7b2123be2d01f105f5f05
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-06 15:41:59 +0600

    :tada: first task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const getEl = el => document.querySelector(el);
+
+const galleryContainer = getEl('.gallery');
+
+const galleryMarkup = galleryItems.map(({ original, description, preview }) => {
+  return `<a class='gallery__item' href='${original}'>
+              <img class='gallery__image' src='${preview}' alt='${description}'>
+           </a>`;
+}).join('');
+
+galleryContainer.innerHTML = galleryMarkup;
+
+function openHandler(event) {
+  event.preventDefault();
+  if (event.target !== event.currentTarget) {
+    new SimpleLightbox('.gallery a', {
+      captionsData: 'alt',
+      captionDelay: 250,
+    });
+  }
+}
+
+galleryContainer.addEventListener('click', openHandler);
