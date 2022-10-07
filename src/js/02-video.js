2eecc5af0dd20ff72156076f403c64aa414bccca
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-10-07 17:28:14 +0600

    :tada: third task completed

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,9 +8,6 @@ function onPlay(data) {
   localStorage.setItem("videoplayer-current-time", `${data.seconds}`)
 }
 
-player.on('play', function() {
-  console.log('played the video!');
-});
 player.on('timeupdate', _.throttle(onPlay, 1000));
 
 player.setCurrentTime(`${localStorage.getItem("videoplayer-current-time")}`).catch(function(error) {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import _ from 'lodash';
 
+const form = document.querySelector('form');
+const emailInput = document.querySelector('[name = email]');
+const messageInput = document.querySelector('[name = message]');
+// const submitBtn = document.querySelector('button[type = submit]');
+
+let obj = {
+  email: '',
+  message: '',
+};
+
+const savedSettings = localStorage.getItem('feedback-form-state');
+
+if (savedSettings !== '') {
+  const parsedSettings = JSON.parse(savedSettings);
+  emailInput.value = parsedSettings.email;
+  messageInput.value = parsedSettings.message;
+}
+
+function onChangeHandler() {
+  obj.email = emailInput.value;
+  obj.message = messageInput.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(obj));
+}
+
+function onSubmitHandler(event) {
+  event.preventDefault();
+  if (emailInput.value === '' || messageInput.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  console.log(obj);
+  localStorage.setItem('feedback-form-state', '');
+  event.currentTarget.reset();
+}
+
+
+form.addEventListener('input', _.throttle(onChangeHandler, 500));
+form.addEventListener('submit', onSubmitHandler);
